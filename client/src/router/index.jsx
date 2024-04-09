33c89f1b82040fac01ecbc4788c30d2eb8a2c603
Ngoc Huy <vongochuy2802@gmail.com>
2024-04-10 00:22:25 +0700

    error page

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -3,6 +3,7 @@ import Login from "../pages/Login";
 import Home from "../pages/Home";
 import AuthProvider from "../context/AuthProvider.jsx";
 import ProtectedRoute from "./ProtectedRoute.jsx";
+import ErorrPage from "../pages/ErrorPage.jsx";
 
 const AuthLayout = () => {
     return <AuthProvider><Outlet /></AuthProvider>
@@ -11,6 +12,7 @@ const AuthLayout = () => {
 export default createBrowserRouter([
     {
         element: <AuthLayout />,
+        errorElement: <ErorrPage />,
         children: [
             {
                 element: <Login />,
