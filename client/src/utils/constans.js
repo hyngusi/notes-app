db882fe5dfb0a65e581e6d2461e867afd084d7c2
Ngoc Huy <vongochuy2802@gmail.com>
2024-04-20 21:07:24 +0700

    update client

--- client/src/utils/constans.js
+++ client/src/utils/constans.js
@@ -1,2 +1,5 @@
-export const GRAPHQL_SERVER = 'http://localhost:4000';
-export const GRAPHQL_SUBSCRIPTIN_ENDPOIN = 'ws://localhost:4000/graphql';
+// export const GRAPHQL_SERVER = 'http://localhost:4000';
+export const GRAPHQL_SERVER = 'https://notes-app-m0ta.onrender.com';
+
+// export const GRAPHQL_SUBSCRIPTIN_ENDPOIN = 'ws://localhost:4000/graphql';
+export const GRAPHQL_SUBSCRIPTIN_ENDPOIN = 'ws:https://notes-app-m0ta.onrender.com/graphql';
