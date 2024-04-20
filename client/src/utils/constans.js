eeb66ab1305ea3943676bf2cb0fccb61f568558f
Ngoc Huy <vongochuy2802@gmail.com>
2024-04-20 21:28:21 +0700

    update endpoint WebSocket

--- client/src/utils/constans.js
+++ client/src/utils/constans.js
@@ -2,4 +2,4 @@
 export const GRAPHQL_SERVER = 'https://notes-app-m0ta.onrender.com';
 
 // export const GRAPHQL_SUBSCRIPTIN_ENDPOIN = 'ws://localhost:4000/graphql';
-export const GRAPHQL_SUBSCRIPTIN_ENDPOIN = 'ws:https://notes-app-m0ta.onrender.com/graphql';
+export const GRAPHQL_SUBSCRIPTIN_ENDPOIN = 'wss://notes-app-m0ta.onrender.com/graphql';
